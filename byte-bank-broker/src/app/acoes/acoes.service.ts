0b95c308df21a5c074f448f8812cf0088e44f4f5
Anderson Silva <andssilva.23@gmail.com>
2022-11-15 01:49:38 -0400

    feat: :rocket: estruturando os dados retornados no método map do rxjs

--- byte-bank-broker/src/app/acoes/acoes.service.ts
+++ byte-bank-broker/src/app/acoes/acoes.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Acao } from './modelo/acoes';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +14,16 @@ export class AcoesService {
 
   getAcoes() {
     return this.http.get<any>('http://localhost:3000/acoes')
+      .pipe(
+        map(acoes => acoes.sort((acaoA, acaoB) => this.ordenaPorCodigo(acaoA, acaoB)))
+      )
+  }
+
+  private ordenaPorCodigo(acaoA: Acao, acaoB: Acao) {
+    if(acaoA.codigo > acaoB.codigo) return 1;
+
+    if(acaoA.codigo < acaoB.codigo) return -1;
+
+    return 0
   }
 }
