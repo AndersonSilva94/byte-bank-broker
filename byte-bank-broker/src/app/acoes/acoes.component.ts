3e1c30d3cb2fc7066944f9a8ba6d62b1d3b513a8
Anderson Silva <andssilva.23@gmail.com>
2022-11-15 01:33:53 -0400

    feat: :rocket: renderizando os dados de ações

--- byte-bank-broker/src/app/acoes/acoes.component.ts
+++ byte-bank-broker/src/app/acoes/acoes.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Acoes } from 'src/app/acoes/modelo/acoes'
+import { AcoesService } from 'src/app/acoes/acoes.service';
 
 @Component({
   selector: 'app-acoes',
   templateUrl: './acoes.component.html',
   styleUrls: ['./acoes.component.css'],
 })
-export class AcoesComponent {
+export class AcoesComponent implements OnInit {
   acoesInput = new FormControl();
+  acoes: Acoes;
 
-  constructor() {}
+  constructor(
+    private acoesService: AcoesService
+  ) {}
+
+  ngOnInit(): void {
+    this.acoesService.getAcoes()
+      .subscribe(
+        res => this.acoes = res.payload
+      )
+  }
 }
